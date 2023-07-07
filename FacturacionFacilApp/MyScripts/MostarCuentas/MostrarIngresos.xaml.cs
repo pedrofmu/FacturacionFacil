860c47f225e0a13f6bf9b24c9c1b5f4ed94680d7
pedroFM <pedrofm3000@gmail.com>
2023-07-07 10:44:21 +0200

    07/07/2023

---
+++ FacturacionFacilApp/MyScripts/MostarCuentas/MostrarIngresos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FacturacionFacilApp.MyScripts.MostarCuentas
+{
+    /// <summary>
+    /// Interaction logic for MostrarIngresos.xaml
+    /// </summary>
+    public partial class MostrarIngresos : Window
+    {
+        public MostrarIngresos()
+        {
+            InitializeComponent();
+        }
+    }
+}
